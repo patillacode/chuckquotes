e3c5247b2789853766183fabb8d0ca6fbe774c19
Gonzalo Fernandez <gonzalo.fernandez@imagination.com>
2016-07-27 12:42:34 +0100

    Chenged default port

--- src/app/random_chuck_server.py
+++ src/app/random_chuck_server.py
@@ -70,9 +70,9 @@ if __name__ == '__main__':
                             default="127.0.0.1",
                             help="IP to run on [default: 127.0.0.1]")
         parser.add_argument("--port",
-                            default=8080,
+                            default=9090,
                             type=int,
-                            help="port to listen to [default: 8080")
+                            help="port to listen to [default: 9090")
         args = parser.parse_args()
 
         app.run(host=args.host, port=args.port)
